d554c4fa00608991cabffed1fe1b52cfadb76543
Kushal Sharma <kdgrtsharma99@gmail.com>
2023-08-21 06:48:06 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,86 +1,41 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = ">= 1.1.0"
   required_providers {
-    azurerm = {
-      version = "~> 3.35"
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.45.0"
+    }
+  }
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "demo-tfe"
+
+    workspaces {
+      name = "manage-tfe"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider "tfe" {
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-demoinfracostor"
-  location = "West Europe"
+resource "tfe_organization" "test-organization" {
+  name  = "demo-tfe-book"
+  email = "admin@company.com"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-demoinfracostor"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "tfe_project" "project" {
+  organization = tfe_organization.test-organization.name
+  name         = "appproject"
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-demoinfracostor"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "tfe_workspace" "wsnetwork" {
+  name         = "network"
+  organization = tfe_organization.test-organization.name
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-demoinfracostor"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
+resource "tfe_workspace" "wsvm" {
+  name         = "vm"
+  organization = tfe_organization.test-organization.name
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                            = "vm-demoinfracostor"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = azurerm_resource_group.rg.location
-  size                            = "Standard_DS4_V2"
-  disable_password_authentication = false
-  admin_username                  = "adminuser"
-  admin_password                  = "P@ssw0rd123*"
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-}
-
-resource "azurerm_service_plan" "plan-app" {
-  name                = "splaninfracostor"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  os_type  = "Linux"
-  sku_name = "B1"
-}
-
-resource "azurerm_linux_web_app" "app" {
-  name                = "webappdemobook1001cor"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan-app.id
-
-  site_config {}
-}
