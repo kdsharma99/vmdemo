65015cfcaf88535f21ece56dda30393085434a8b
Kushal Sharma <kdgrtsharma99@gmail.com>
2023-08-21 06:42:15 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
+  required_version = "~> 1.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.35"
     }
   }
 }
@@ -12,15 +12,75 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-demo-opa"
-  location = "westeurope"
+  name     = "rg-demoinfracostor"
+  location = "West Europe"
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                      = "sademotestopa1"
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = azurerm_resource_group.rg.location
-  account_tier              = "Standard"
-  account_replication_type  = "GRS"
-  enable_https_traffic_only = false #true
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-demoinfracostor"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-demoinfracostor"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-demoinfracostor"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "vm-demoinfracostor"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_DS4_V2"
+  disable_password_authentication = false
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd123*"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_service_plan" "plan-app" {
+  name                = "splaninfracostor"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "webappdemobook1001cor"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan-app.id
+
+  site_config {}
 }
